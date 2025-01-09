9a2ea095793d68bb8678bedcefabee45b9e3390c
Kaustubh070707 <k9523604@gmail.com>
2025-01-09 16:27:12 +0530

    Setup Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import Navbar from "./components/Navbar";
+import Body from "./components/Body";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      Hello World
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
